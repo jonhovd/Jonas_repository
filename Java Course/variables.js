625a1b75cd49cce092589ef94f708e03cac6ce22
jonhovd <jonhovd@outlook.com>
2022-09-17 18:53:47 +0200

    Øver på js

--- Java Course/variables.js
+++ Java Course/variables.js
@@ -4,12 +4,12 @@ let age; //numbers
 age = 23;
 let student = true; //booleans
 
-age = age + 1;
+age = age + 3;
 
 console.log("Hei", firstname);
-console.log("Du er", age, "gammel");
+console.log("Du er", age, "år", "gammel");
 console.log("Enrolled", student);
 
 document.getElementById("p1").innerHTML = "Hei " + firstname;
-document.getElementById("p2").innerHTML = "Du er " + age + " gammel";
+document.getElementById("p2").innerHTML = "Du er " + age + " år" + " gammel";
 document.getElementById("p3").innerHTML = "Enrolled " + student;
