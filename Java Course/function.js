50438f98765d4740db996b2937e60b1bc65e8376
jonhovd <jonhovd@outlook.com>
2022-09-18 21:46:19 +0200

    Øver på js

---
+++ Java Course/function.js
@@ -0,0 +1,16 @@
+
+startProgram();
+
+function startProgram() {
+    let userName = "Jonas";
+    let age = 23;
+    happyBirthday(userName, age);
+}
+
+function happyBirthday(userName, age){
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear", userName);
+    console.log("Happy birthday to you!");
+    console.log("You are", age, "years old!");
+}
