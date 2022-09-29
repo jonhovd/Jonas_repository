ec5fe1132888299ca36a9dd93bb241e0b0ae7946
jonhovd <jonhovd@outlook.com>
2022-09-29 21:04:32 +0200

    js string

---
+++ Java Course/string.js
@@ -0,0 +1,17 @@
+// useful string properties
+
+let userName = " JonHovd  ";
+let phoneNumber ="454-55-059";
+
+//console.log(userName.length);
+//console.log(userName.charAt(0));
+//console.log(userName.indexOf("o"));
+//console.log(userName.lastIndexOf("o"));
+//userName = userName.trim()
+//userName = userName.toUpperCase();
+//userName = userName.toLowerCase();
+
+
+phoneNumber = phoneNumber.replaceAll("-","");
+
+console.log(phoneNumber);
