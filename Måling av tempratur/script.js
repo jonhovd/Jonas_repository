536cd7ac99babc8a6cc8999c57d12d8660062cea
jonhovd <jonhovd@outlook.com>
2022-10-14 15:46:23 +0200

    Oppsett på tmp36 måling

--- Måling av tempratur/script.js
+++ Måling av tempratur/script.js
@@ -1,2 +1,7 @@
-document.getElementById('aLabel')
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
+    document.getElementById("outputFarenheit").innerHTML=
+    document.getElementById("outputKelvin").innerHTML=(valNum-32)/1.8 + 273.15;
+}
 
